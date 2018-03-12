b56e54e80752457373748fee944cd72391b6104c
Hector Norza <norzagaray@me.com>
2018-03-12 15:25:37 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 express()
   .use(express.static(`${__dirname}/build`))
+  .get("*", (req, res) => res.sendFile(`${__dirname}/build/index.html`))
   .listen(process.env.PORT, () =>
     console.log("__SERVER_RUNNING__", process.env.PORT)
   );
